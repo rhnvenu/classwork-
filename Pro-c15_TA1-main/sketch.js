f7c85900b031a2fb5833bcefc1305b2febdfdb91
rhnvenu <109427183+rhnvenu@users.noreply.github.com>
2022-07-29 10:59:46 -0400

    Add files via upload

---
+++ Pro-c15_TA1-main/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  count()
+}
+
+function count()
+{
+  let numbers = [1,2,3,4,5,6,7,8,9];
+  let len = numbers.length;
+
+  for(let i = 0; i<len; i++)
+  {
+    console.log(numbers[i]);
+  }
+}
